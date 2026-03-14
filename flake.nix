e64683b7244a2b912f47a7cae2217af37b9a0b6b
lptlv <119885342+lptlv@users.noreply.github.com>
2026-03-14 18:26:33 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,18 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
+    in
       {
-        packages.default = pkgs.callPackage ./msnap.nix { };
-      }
-    );
+        packages = forAllSystems (system: {
+          default = nixpkgs.${system}.callPackage ./nix/msnap.nix { };
+      });
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/msnap";
+        };
+      });
+
+      nixosModules.default = { config, lib, pkgs, ... }:
+        let
+          cfg = config.programs.msnap;
+          msnapPkg = self.packages.${pkgs.system}.default;
+        in
+          {
+          imports = [ ./nix/nixos-module.nix ];
+
+          config = lib.mkIf cfg.enable {
+            programs.msnap.package = lib.mkDefault msnapPkg;
+          };
+        };
 }
