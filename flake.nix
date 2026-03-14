688e46fe1a85ea34991fb588bbe84b2674f4313b
lptlv <119885342+lptlv@users.noreply.github.com>
2026-03-14 19:29:46 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,45 +1,37 @@
 {
-  description = "Screenshot and screencast utility for mangowm";
+  description = "msnap flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, flake-parts, nixpkgs } @ inputs:
-      flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [
-        inputs.flake-parts.flakeModules.easyOverlay
-      ];
+  outputs = { self, nixpkgs, msnap-src }:
+  let
+    systems = [ "x86_64-linux" "aarch64-linux" ];
 
-      flake = {
-        nixosModules.msnap = import ./nix/nixos-modules.nix self;
-      };
-
-      perSystem = {
-        config,
-        pkgs,
-        ...
-      }: let
-        inherit (pkgs) callPackage ;
-        msnap = callPackage ./nix/msnap.nix {};
-        shellOverride = old: {
-          nativeBuildInputs = old.nativeBuildInputs ++ [];
-          buildInputs = old.buildInputs ++ [];
-        };
-      in {
-        packages.default = msnap;
-        overlayAttrs = {
-          inherit (config.packages) msnap;
-        };
-        packages = {
-          inherit msnap;
-        };
-        devShells.default = msnap.overrideAttrs shellOverride;
-      };
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs systems (system:
+        f (import nixpkgs { inherit system; }));
+  in
+  {
+    overlays.default = final: prev: {
+      msnap = final.callPackage "./nix/msnap.nix" {};
     };
+
+    packages = forAllSystems (pkgs:
+      let
+        pkgs' = pkgs.extend self.overlays.default;
+      in
+      {
+        msnap = pkgs'.msnap;
+        default = pkgs'.msnap;
+      });
+
+    apps = forAllSystems (pkgs: {
+      msnap = {
+        type = "app";
+        program = "${self.packages.${pkgs.system}.msnap}/bin/msnap";
+      };
+    });
+  };
 }
